dc9b90efb2d1de4ba26a91f5ce164474eb6c926b
Will Ho <ikaruswill.ho@gmail.com>
2021-03-17 12:52:52 +0800

    Fix typo from testing
    Also remove duplicated log message

--- src/scripts/auto_backup.sh
+++ src/scripts/auto_backup.sh
@@ -12,12 +12,12 @@ log "Starting auto backup process..."
 
 if [ "${AUTO_BACKUP_REMOVE_OLD:=0}" -eq 1 ]; then
     log "Removing old backups..."
-    find /home/steam/backups -mtime +$((${AUTO_BACKUP_DAYS_TO_LIVE:-5} - 1)) -exec echo {} \;
+    find /home/steam/backups -mtime +$((${AUTO_BACKUP_DAYS_TO_LIVE:-5} - 1)) -exec rm {} \;
 fi
 
+log "Creating backup..."
 file_name="$(date +"%Y%m%d-%H%M%S")-${1:-"backup"}.tar.gz"
 
-log "Starting auto backup process..."
 odin backup /home/steam/.config/unity3d/IronGate/Valheim "/home/steam/backups/${file_name}" || exit 1
 
 log "Backup process complete! Created ${file_name}"
