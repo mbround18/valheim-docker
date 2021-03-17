41755f1629d43eb0823d657cf5aabac6bb57255e
Will Ho <ikaruswill.ho@gmail.com>
2021-03-17 12:50:57 +0800

    Move backup creation step to after backups are pruned
    To handle case where AUTO_BACKUP_DAYS_TO_LIVE == 0 deletes all backups including latest.
    New behaviour would be to only retain latest backup and delete all others.

--- src/scripts/auto_backup.sh
+++ src/scripts/auto_backup.sh
@@ -8,14 +8,16 @@ log() {
   printf "%-16s: %s\n" "${PREFIX}" "$1"
 }
 
-file_name="$(date +"%Y%m%d-%H%M%S")-${1:-"backup"}.tar.gz"
-
 log "Starting auto backup process..."
-odin backup /home/steam/.config/unity3d/IronGate/Valheim "/home/steam/backups/${file_name}" || exit 1
 
 if [ "${AUTO_BACKUP_REMOVE_OLD:=0}" -eq 1 ]; then
     log "Removing old backups..."
     find /home/steam/backups -mtime +$((${AUTO_BACKUP_DAYS_TO_LIVE:-5} - 1)) -exec echo {} \;
 fi
+
+file_name="$(date +"%Y%m%d-%H%M%S")-${1:-"backup"}.tar.gz"
+
+log "Starting auto backup process..."
+odin backup /home/steam/.config/unity3d/IronGate/Valheim "/home/steam/backups/${file_name}" || exit 1
 
 log "Backup process complete! Created ${file_name}"
