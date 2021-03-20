7a46d278110a1df358b6463bee96f9ea55040526
Will Ho <ikaruswill@users.noreply.github.com>
2021-03-20 15:02:05 +0800

    Subtract 1 from AUTO_BACKUP_DAYS_TO_LIVE and prune before creating backup (#290)
    * Remove + from find -mtime
    
    Fixes #289
    
    * Revert "Remove + from find -mtime"
    
    This reverts commit 08af441b798fe615d4aa5e5aa617de4b6c31f674.
    
    * Subtract 1 from AUTO_BACKUP_DAYS_TO_LIVE
    
    Before passing to find mtime argument as +${AUTO_BACKUP_DAYS_TO_LIVE}
    Also add log message to indicate old backup removal
    
    * Move backup creation step to after backups are pruned
    
    To handle case where AUTO_BACKUP_DAYS_TO_LIVE == 0 deletes all backups including latest.
    New behaviour would be to only retain latest backup and delete all others.
    
    * Fix typo from testing
    
    Also remove duplicated log message

--- src/scripts/auto_backup.sh
+++ src/scripts/auto_backup.sh
@@ -8,13 +8,16 @@ log() {
   printf "%-16s: %s\n" "${PREFIX}" "$1"
 }
 
-file_name="$(date +"%Y%m%d-%H%M%S")-${1:-"backup"}.tar.gz"
-
 log "Starting auto backup process..."
-odin backup /home/steam/.config/unity3d/IronGate/Valheim "/home/steam/backups/${file_name}" || exit 1
 
 if [ "${AUTO_BACKUP_REMOVE_OLD:=0}" -eq 1 ]; then
-    find /home/steam/backups -mtime +${AUTO_BACKUP_DAYS_TO_LIVE:-5} -exec rm {} \;
+    log "Removing old backups..."
+    find /home/steam/backups -mtime +$((${AUTO_BACKUP_DAYS_TO_LIVE:-5} - 1)) -exec rm {} \;
 fi
+
+log "Creating backup..."
+file_name="$(date +"%Y%m%d-%H%M%S")-${1:-"backup"}.tar.gz"
+
+odin backup /home/steam/.config/unity3d/IronGate/Valheim "/home/steam/backups/${file_name}" || exit 1
 
 log "Backup process complete! Created ${file_name}"
