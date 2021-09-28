02971e3589f40f87508e4f9f031590ed82ac4cd6
Michael <12646562+mbround18@users.noreply.github.com>
2021-09-28 13:22:41 -0700

    Fix sysinfo (#454)
    * Fix sysinfo
    
    * dep

--- src/odin/server/process.rs
+++ src/odin/server/process.rs
@@ -13,20 +13,18 @@ impl ServerProcess {
     }
   }
 
-  pub fn get_valheim_processes(&mut self) -> Vec<&Process> {
+  pub fn valheim_processes(&mut self) -> Vec<&Process> {
     self.system.refresh_processes();
 
     // Limit search string to 15 characters, as some unix operating systems
     // cannot handle more then 15 character long process names
     let valheim_executable_search_name = &constants::VALHEIM_EXECUTABLE_NAME[..15];
 
-    self
-      .system
-      .get_process_by_name(valheim_executable_search_name)
+    self.system.process_by_name(valheim_executable_search_name)
   }
 
-  pub fn is_running(&mut self) -> bool {
-    let valheim_processes = self.get_valheim_processes();
-    !valheim_processes.is_empty()
+  //noinspection RsSelfConvention
+  pub fn is_process_running(&mut self) -> bool {
+    !self.valheim_processes().is_empty()
   }
 }
--- src/odin/server/utils.rs
+++ src/odin/server/utils.rs
@@ -1,5 +1,5 @@
 use super::process::ServerProcess;
 
 pub fn is_running() -> bool {
-  ServerProcess::new().is_running()
+  ServerProcess::new().is_process_running()
 }
