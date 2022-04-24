9694bf6fa50d39a8da2ed7ac814b9a5b71149f34
mbround18 <12646562+mbround18@users.noreply.github.com>
2022-04-24 00:44:08 -0700

    Env Length Fix

---
+++ src/scripts/env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#################################
+# AUTO GENERATED; DO NOT MODIFY #
+#################################
+
+. /etc/profile
+. ~/.bash_profile
+. ~/.bashrc
+
+#################################
+# AUTO GENERATED; DO NOT MODIFY #
+#################################
