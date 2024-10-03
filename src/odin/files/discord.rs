d5f15ada3d860282772a37054967092521893ffe
mbround18 <michael.bruno1337@gmail.com>
2024-10-02 18:39:33 -0700

    Initial setup for player join/leave webhook, need to modify logs command

--- src/odin/files/discord.rs
+++ src/odin/files/discord.rs
@@ -1,6 +1,6 @@
 use crate::{
   files::{FileManager, ManagedFile},
-  notifications::discord::{body_template, DiscordWebHookBody},
+  notifications::discord::{DiscordWebHookBody},
   utils::{environment::fetch_var, path_exists},
 };
 
@@ -16,6 +16,8 @@ pub struct DiscordConfigEvents {
   start: DiscordWebHookBody,
   stop: DiscordWebHookBody,
   update: DiscordWebHookBody,
+  player_join: DiscordWebHookBody,
+  player_leave: DiscordWebHookBody,
 }
 
 #[derive(Deserialize, Serialize)]
@@ -24,22 +26,19 @@ pub struct DiscordConfig {
 }
 
 fn basic_template() -> DiscordConfig {
-  let events: HashMap<String, DiscordWebHookBody> = HashMap::new();
-  let mut config = DiscordConfig { events };
-  config
-    .events
-    .insert(String::from("broadcast"), body_template());
-  config.events.insert(String::from("start"), body_template());
-  config.events.insert(String::from("stop"), body_template());
-  config
-    .events
-    .insert(String::from("update"), body_template());
-  config
+  let mut events: HashMap<String, DiscordWebHookBody> = HashMap::new();
+  events.insert(String::from("broadcast"), DiscordWebHookBody::default());
+  events.insert(String::from("start"), DiscordWebHookBody::default());
+  events.insert(String::from("stop"), DiscordWebHookBody::default());
+  events.insert(String::from("update"), DiscordWebHookBody::default());
+  events.insert(String::from("player_join"), DiscordWebHookBody::default());
+  events.insert(String::from("player_leave"), DiscordWebHookBody::default());
+  DiscordConfig { events }
 }
 
 pub fn load_discord() -> DiscordConfig {
   let file = discord_file();
-  read_discord(file)
+  read_discord(&file)
 }
 
 pub fn discord_file() -> ManagedFile {
@@ -48,25 +47,216 @@ pub fn discord_file() -> ManagedFile {
   ManagedFile { name }
 }
 
-pub fn read_discord(discord: ManagedFile) -> DiscordConfig {
+pub fn read_discord(discord: &dyn FileManager) -> DiscordConfig {
+  let default = basic_template();
   let content = discord.read();
   if content.is_empty() {
-    basic_template()
-  } else {
-    serde_json::from_str(content.as_str()).unwrap()
+    return default;
   }
+
+  let config =
+    serde_json::from_str::<HashMap<String, HashMap<String, DiscordWebHookBody>>>(content.as_str())
+      .unwrap()
+      .get("events")
+      .unwrap_or(&default.events)
+      .clone();
+  let mut events: HashMap<String, DiscordWebHookBody> = HashMap::new();
+
+  for (key, value) in default.events {
+    if !config.contains_key(&key) {
+      {
+        events.insert(key, value);
+      }
+    } else if let Some(return_value) = config.get(&key) {
+      events.insert(key, return_value.clone());
+    } else {
+      events.insert(key, DiscordWebHookBody::default());
+    }
+  }
+
+  DiscordConfig { events }
 }
 
-pub fn write_discord(discord: ManagedFile) -> bool {
-  let notification = basic_template();
-  let content_to_write = serde_json::to_string_pretty(&notification).unwrap();
-  debug!(
-    "Writing discord config: \n{}",
-    serde_json::to_string_pretty(&notification).unwrap()
-  );
+pub fn write_discord(discord: &dyn FileManager) -> bool {
   if path_exists(&discord.path()) {
-    false
-  } else {
-    discord.write(content_to_write)
+    debug!("Discord config file already exists, doing nothing.");
+    return true; 
+  }
+
+  let template_notification = basic_template();
+  let content_to_write = serde_json::to_string_pretty(&template_notification).unwrap();
+  debug!("Writing discord config: \n{}", content_to_write);
+
+  discord.write(content_to_write)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use mockall::*;
+
+  mock! {
+      ManagedFile {
+          fn read(&self) -> String;
+          fn write(&self, content: String) -> bool;
+          fn path(&self) -> String;
+      }
+  }
+
+  impl FileManager for MockManagedFile {
+    fn path(&self) -> String {
+      self.path()
+    }
+
+    fn read(&self) -> String {
+      self.read()
+    }
+
+    fn write(&self, content: String) -> bool {
+      self.write(content)
+    }
+  }
+
+  #[test]
+  fn test_basic_template() {
+    let config = basic_template();
+    assert!(config.events.contains_key("broadcast"));
+    assert!(config.events.contains_key("start"));
+    assert!(config.events.contains_key("stop"));
+    assert!(config.events.contains_key("update"));
+    assert!(config.events.contains_key("player_join"));
+    assert!(config.events.contains_key("player_leave"));
+  }
+
+  #[test]
+  fn test_read_discord_with_empty_content() {
+    let mut mock_file = MockManagedFile::new();
+    mock_file.expect_read().return_const(String::from(""));
+
+    let config = read_discord(&mock_file);
+
+    assert!(config.events.contains_key("broadcast"));
+    assert!(config.events.contains_key("start"));
+    assert!(config.events.contains_key("stop"));
+    assert!(config.events.contains_key("update"));
+    assert!(config.events.contains_key("player_join"));
+    assert!(config.events.contains_key("player_leave"));
+  }
+
+  #[test]
+  fn test_read_discord_with_valid_content() {
+    let content = r#"
+        {
+            "events": {
+                "broadcast": {
+                    "content": "test_broadcast",
+                    "embeds": [{
+                        "title": "Test Title",
+                        "description": "Test Description",
+                        "color": 12345
+                    }]
+                }
+            }
+        }"#;
+
+    let mut mock_file = MockManagedFile::new();
+    mock_file.expect_read().return_const(String::from(content));
+
+    let config = read_discord(&mock_file);
+
+    assert_eq!(
+      config.events.get("broadcast").unwrap().content,
+      "test_broadcast"
+    );
+    assert_eq!(
+      config.events.get("broadcast").unwrap().embeds[0].title,
+      "Test Title"
+    );
+    assert_eq!(
+      config.events.get("broadcast").unwrap().embeds[0].description,
+      "Test Description"
+    );
+    assert_eq!(
+      config.events.get("broadcast").unwrap().embeds[0].color,
+      12345
+    );
+  }
+
+  #[test]
+  fn test_write_discord_when_path_does_not_exist() {
+    let mut mock_file = MockManagedFile::new();
+    mock_file
+      .expect_write()
+      .withf(|content| content.contains("broadcast"))
+      .return_const(true);
+
+    mock_file
+      .expect_path()
+      .return_const(String::from("non_existing_path"));
+
+    let result = write_discord(&mock_file);
+    assert!(
+      result,
+      "Expected write to return true when path does not exist."
+    );
+  }
+
+  #[test]
+  fn test_read_discord_without_events_key() {
+    let mut mock_file = MockManagedFile::new();
+    let no_events_content = r#"{}"#;
+    mock_file.expect_read().return_const(String::from(no_events_content));
+
+    let config = read_discord(&mock_file);
+
+    assert!(config.events.contains_key("broadcast"));
+    assert!(config.events.contains_key("start"));
+  }
+
+
+  #[test]
+  fn test_read_discord_with_extra_event_keys() {
+    let mut mock_file = MockManagedFile::new();
+    let extra_keys_content = r#"
+    {
+        "events": {
+            "extra_event": {
+                "title": "test",
+                "content": "extra_content",
+                "embeds": [{
+                    "title": "title",
+                    "description": "Test Description",
+                    "color": 12345
+                }]
+            }
+        }
+    }"#;
+    mock_file.expect_read().return_const(String::from(extra_keys_content));
+
+    let config = read_discord(&mock_file);
+
+    assert!(!config.events.contains_key("extra_event"), "Unexpected key should be ignored");
+    assert!(config.events.contains_key("broadcast"));
+    assert!(config.events.contains_key("start"));
+  }
+
+
+
+  #[test]
+  fn test_read_discord_with_malformed_json() {
+    let mut mock_file = MockManagedFile::new();
+    let malformed_json = r#"{ "events": { "broadcast": { "content": "test_broadcast""#;
+    mock_file.expect_read().return_const(String::from(malformed_json));
+
+    let result = std::panic::catch_unwind(|| read_discord(&mock_file));
+
+    assert!(result.is_err(), "Expected a panic when the JSON is malformed");
+  }
+
+
+  #[test]
+  fn test_discord_file() {
+    let managed_file = discord_file();
+    assert_eq!(managed_file.name, "discord.json");
   }
 }
--- src/odin/notifications/discord.rs
+++ src/odin/notifications/discord.rs
@@ -1,17 +1,19 @@
 use crate::files::discord::load_discord;
-
 use crate::notifications::enums::event_status::EventStatus;
 use crate::notifications::enums::notification_event::parse_server_name_for_notification;
+use crate::notifications::enums::player::PlayerStatus;
 use crate::notifications::NotificationMessage;
 use handlebars::Handlebars;
 use log::debug;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Color {
   Success = 0x4B_B5_43,
   Failure = 0xFA_11_3D,
   Generic = 0x00_7F_66,
+  Join = 0x34_98_DB,
+  Leave = 0xE7_4C_3C,
 }
 
 const DISCORD_WEBHOOK_BASE: &str = "https://discord.com/api/webhooks";
@@ -28,6 +30,16 @@ impl From<EventStatus> for Color {
   }
 }
 
+impl From<PlayerStatus> for Color {
+  fn from(status: PlayerStatus) -> Self {
+    use PlayerStatus::{Joined, Left};
+    match status {
+      Joined => Self::Join,
+      Left => Self::Leave,
+    }
+  }
+}
+
 pub fn is_discord_webhook(webhook_url: &str) -> bool {
   webhook_url.starts_with(DISCORD_WEBHOOK_BASE) || webhook_url.starts_with(DISCORDAPP_WEBHOOK_BASE)
 }
@@ -39,20 +51,41 @@ pub struct DiscordWebHookEmbed {
   pub(crate) color: i32,
 }
 
+impl Clone for DiscordWebHookEmbed {
+  fn clone(&self) -> Self {
+    DiscordWebHookEmbed {
+      title: String::from(&self.title),
+      description: String::from(&self.description),
+      color: self.color,
+    }
+  }
+}
+
 #[derive(Deserialize, Serialize)]
 pub struct DiscordWebHookBody {
   pub(crate) content: String,
   pub(crate) embeds: Vec<DiscordWebHookEmbed>,
 }
 
-pub fn body_template() -> DiscordWebHookBody {
-  DiscordWebHookBody {
-    content: "Notification: {{server_name}}".to_string(),
-    embeds: vec![DiscordWebHookEmbed {
-      title: "{{title}}".to_string(),
-      description: "{{description}}".to_string(),
-      color: 16388413,
-    }],
+impl Clone for DiscordWebHookBody {
+  fn clone(&self) -> Self {
+    DiscordWebHookBody {
+      content: String::from(&self.content),
+      embeds: self.embeds.clone(),
+    }
+  }
+}
+
+impl Default for DiscordWebHookBody {
+  fn default() -> Self {
+    DiscordWebHookBody {
+      content: "Notification: {{server_name}}".to_string(),
+      embeds: vec![DiscordWebHookEmbed {
+        title: "{{title}}".to_string(),
+        description: "{{description}}".to_string(),
+        color: 16388413,
+      }],
+    }
   }
 }
 
@@ -81,7 +114,7 @@ impl From<&NotificationMessage> for DiscordWebHookBody {
   fn from(event: &NotificationMessage) -> Self {
     let discord_file = load_discord();
     let mut handlebars = Handlebars::new();
-    let default_event = body_template();
+    let default_event = DiscordWebHookBody::default();
     let discord_event = &discord_file
       .events
       .get(&event.event_type.name.as_str().to_lowercase())
@@ -105,5 +138,56 @@ impl From<&NotificationMessage> for DiscordWebHookBody {
       Err(msg) => panic!("{}", msg.to_string()),
     };
     serde_json::from_str(&rendered).unwrap()
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::notifications::enums::event_status::EventStatus;
+  use crate::notifications::enums::notification_event::NotificationEvent;
+  use crate::notifications::enums::player::PlayerStatus;
+  use crate::notifications::NotificationMessage;
+  use chrono::Local;
+
+  #[test]
+  fn test_color_from_event_status() {
+    assert_eq!(Color::from(EventStatus::Successful), Color::Success);
+    assert_eq!(Color::from(EventStatus::Failed), Color::Failure);
+    assert_eq!(Color::from(EventStatus::Running), Color::Generic);
+  }
+
+  #[test]
+  fn test_color_from_player_status() {
+    assert_eq!(Color::from(PlayerStatus::Joined), Color::Join);
+    assert_eq!(Color::from(PlayerStatus::Left), Color::Leave);
+  }
+
+  #[test]
+  fn test_body_template() {
+    let template = DiscordWebHookBody::default();
+    assert_eq!(template.content, "Notification: {{server_name}}");
+    assert_eq!(template.embeds.len(), 1);
+    assert_eq!(template.embeds[0].title, "{{title}}");
+    assert_eq!(template.embeds[0].description, "{{description}}");
+    assert_eq!(template.embeds[0].color, 16388413);
+  }
+
+  #[test]
+  fn test_discord_webhook_body_from_notification_message() {
+    let notification = NotificationMessage {
+      author: String::from("Test Author"),
+      event_type: NotificationEvent::Player(PlayerStatus::Joined).to_event_type(),
+      event_message: String::from("Player has joined the game."),
+      timestamp: Local::now().to_rfc3339(),
+    };
+
+    let discord_body: DiscordWebHookBody = (&notification).into();
+    assert_eq!(discord_body.embeds.len(), 1);
+    assert_eq!(discord_body.embeds[0].title, "Player");
+    assert_eq!(
+      discord_body.embeds[0].description,
+      "Player has joined the game."
+    );
   }
 }
